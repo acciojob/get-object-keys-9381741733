b9bf6bfaf2de31f5bf75fc250926822333b1eb1a
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-02-27 19:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-// Create the student object
 const student = {
   name: "John",
 };
 
-// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
-// Testing the function
-console.log(getKeys(student)); // Output: ["name"]
-
-// Additional Test Cases
-const student2 = {
-  name: "Alice",
-  age: 22,
-  city: "New York"
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
 };
 
-console.log(getKeys(student2)); // Output: ["name", "age", "city"]
-
-const student3 = {
-  id: 101,
-  grade: "A",
-};
-
-console.log(getKeys(student3)); // Output: ["id", "grade"]
+// Do not change code below this line
+window.student = student;
+window.getKeys = getKeys;
